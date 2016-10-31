e8091f54ee428c8fcd766a251a9918f2a507df8e
Kevin Wang <wangkevin448@gmail.com>
2016-10-31 16:08:22 -0700

    update normal distribution for random init

--- linreg.py
+++ linreg.py
@@ -11,7 +11,7 @@ class LinReg:
         weight: vertical vector of size + 1
         '''
         assert size > 0
-        self._weight = np.random.normal(scale=0.5, size=(size + 1, 1,))
+        self._weight = np.random.normal(size=(size + 1, 1,))
 
     def reshape_inputs(self, inputs):
         return np.append(inputs, np.ones((inputs.shape[0], 1,)), axis=1)
@@ -31,7 +31,7 @@ class LinReg:
         alpha: training rate
         '''
         h = self.run(inputs)
-        delta_weight = np.dot(self.reshape_inputs(inputs).T, h - outputs)*(-alpha/inputs.shape[0])
+        delta_weight = np.dot(self.reshape_inputs(inputs).T, h - outputs) * (-alpha / inputs.shape[0])
         self._weight += delta_weight
 
     def cost(self, out_h, out_actual):
@@ -40,4 +40,4 @@ class LinReg:
         out_h: numpy vertical vector of output from run
         out_actual: numpy vertical vector of actual output
         '''
-        return np.sum((out_h - out_actual)**2)/2/out_h.shape[0]
+        return np.sum((out_h - out_actual)**2) / 2 / out_h.shape[0]
