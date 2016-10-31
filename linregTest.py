bb7b9f463fd7f6e60208f6fc557ae0794790019b
Kevin Wang <wangkevin448@gmail.com>
2016-10-31 16:34:22 -0700

    prettified printing

--- linregTest.py
+++ linregTest.py
@@ -2,15 +2,21 @@ import math as math
 import numpy as np
 from linreg import LinReg
 
-iterations = 65536
+iterations = 98304
 print_rate = 8192
-training_set_size = 128
-batch_size = 4
+training_set_size = 2048
+batch_size = 32
 
 training_set_inputs = np.random.uniform(high=16, size=(training_set_size, 2,))
 training_set_outputs = training_set_inputs[:, 0, np.newaxis] + 2 * training_set_inputs[:, 1, np.newaxis] + 3
 
 linReg = LinReg(2)
+
+print('{:>10} | {:<24} | {:<24}'.format('iteration', 'cost', 'test input [[1, 2]]'))
+s = ''
+for i in range(10 + 24 + 24 + 2 * 3):
+    s += '-'
+print(s)
 
 for i in range(iterations):
     k = math.floor(i % training_set_size / batch_size)
@@ -19,10 +25,13 @@ for i in range(iterations):
     linReg.train(batch, batch_out, 0.001)
     # linReg.train(training_set_inputs, training_set_outputs, 0.0001)
     if(i % print_rate == 0):
-        print('iteration {:>8} | cost: {:>8}'.format(i, linReg.cost(linReg.run(batch), batch_out)), flush=True)
+        print(
+            '{:>10} | {:<24} | {:<24}'
+            .format(i, linReg.cost(linReg.run(batch), batch_out), linReg.run(np.array([[1, 2]]))[0, 0]),
+            flush=True)
 
 print('final weight')
 print(linReg._weight)
 
-print('final test')
-print(linReg.run(np.array([[1, 2]])))
+print('final test:')
+print(linReg.run(np.array([[1, 2]]))[0, 0])
